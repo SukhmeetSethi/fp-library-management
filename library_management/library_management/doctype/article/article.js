21da33871fac76c21235a85b241c450fc46b835c
sukhmeetsethi <sukhmeet.sethi@gmail.com>
2025-03-07 12:37:47 +0000

    added docType Article

---
+++ library_management/library_management/doctype/article/article.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sukhmeet Sethi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Article", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library_management/library_management/doctype/article/article.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Sukhmeet Sethi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Article(Document):
+	pass
